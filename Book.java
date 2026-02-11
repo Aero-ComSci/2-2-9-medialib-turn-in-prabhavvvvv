c683ab2cfd6d7736a416cf86220df3955ce4667c
prabhaaaav <108563497+prabhavvvvv@users.noreply.github.com>
2026-02-10 20:28:49 -0800

    Update Book.java

--- Book.java
+++ Book.java
@@ -9,49 +9,57 @@ public class Book
   private String author;
   //only mutated by value
   private int rating;
-  
+ 
   /*** Constructor ****/
   public Book(String t, String a)
   {
     title = t;
     author = a;
     rating = 0;
-    
   }
 
-  
+
+ 
    /*** Accessor methods ***/
   public String getTitle() {
     return title;
   }
 
+
   public String getAuthor() {
     return author;
   }
-  
+ 
   public int getRating() {
     return rating;
   }
-  
-  
-  public String toString() 
+ 
+ 
+  public String toString()
   {
     String info = "\"" + title + "\", written by " + author;
-    if (rating != 0) 
-    { 
+    if (rating != 0)
+    {
       info += ", rating is " + rating;
     }
     return info;
   }
+
+
+
+
+
 
   /*** Mutator methods ***/
   public void setTitle(String t) {
     title = t;
   }
 
+
   public void setAuthor(String a) {
     author = a;
   }
+
 
   /**
    * Set rating
@@ -61,11 +69,14 @@ public class Book
    * precondition: object cannot be null
    */
 
+
   public void adjustRating(int r) {
     if (r >=0 && r <=10) {
       rating = r;
     }
+    System.out.println("Adjusting rating by " + r);
   }
+
 
   public boolean equals(Book otherBook) {
     if (this.title.equals(otherBook.title) && this.author.equals(otherBook.author)) {
