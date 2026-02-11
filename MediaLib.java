5e8bf3efe7fe383be16ccd5a03cc751b6f053705
prabhaaaav <108563497+prabhavvvvv@users.noreply.github.com>
2026-02-10 20:30:10 -0800

    Create MediaLib.java

---
+++ MediaLib.java
@@ -0,0 +1,106 @@
+/*
+ * Activity 2.2.9
+ */
+public class MediaLib
+{
+  public static String owner = "Prabhav Yeddanapudi";
+  
+  private static int numEntries;
+  private static int numBooks;
+  private static int numMovies;
+  private static int numSongs;
+  private String lastModified;
+  
+  private Book book;
+  private Movie movie;
+  private Song song;
+  
+  public MediaLib() {
+    lastModified = DateTime.getTime();
+  }
+  
+  public void addBook(Book b)
+  {
+    if (book == null)
+    {
+      book = b;
+      numEntries++;
+      numBooks++;
+      lastModified = DateTime.getTime();
+    } 
+    else
+      System.out.println("Cannot add a new book: A book already exists");
+  }
+  
+  public void addMovie(Movie m)
+  {
+    if (movie == null)
+    {
+      movie = m;
+      numEntries++;
+      numMovies++;
+      lastModified = DateTime.getTime();
+    }
+    else
+      System.out.println("Cannot add a new movie: A movie already exists");
+  }
+  
+  public void addSong(Song s)
+  {
+    if (song == null)
+    {
+      song = s;
+      numEntries++;
+      numSongs++;
+      lastModified = DateTime.getTime();
+    }
+    else
+      System.out.println("Cannot add a new song: A song already exists");
+  }
+
+  public static String getOwner()
+  {
+    return owner;
+  }
+
+  public static void changeOwner(String o)
+  {
+    owner = o;
+  }
+  
+  public static int getNumEntries()
+  {
+    return numEntries;
+  }
+  
+  public static int getNumBooks()
+  {
+    return numBooks;
+  }
+  
+  public static int getNumMovies()
+  {
+    return numMovies;
+  }
+  
+  public static int getNumSongs()
+  {
+    return numSongs;
+  }
+  
+  public String toString() 
+  {
+    String info = "";
+    
+    if (book != null)
+      info += "Book: " + book + "\n";
+    if (movie != null)
+      info += "Movie: " + movie + "\n";
+    if (song != null)
+      info += "Song: " + song + "\n";
+
+    info += "Last Modified: " + lastModified + "\n";
+ 
+    return info;
+  }
+}
